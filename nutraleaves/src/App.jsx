cc859cac903e1de1f34ede72fcf46a44dafa18cc
Abhinav mohanan <abhinavmohanan018@gmail.com>
2026-01-16 12:02:45 +0530

    chore: Initialize react project structure

---
+++ nutraleaves/src/App.jsx
@@ -0,0 +1,14 @@
+import { User } from "lucide-react"
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-500'>Vite + React</h1>
+      <User className="w-4 h-4"/>
+    </>
+  )
+}
+
+export default App
