50d22f66d66c712f026d66bfcae60ad10f9ad7f2
Abhinav mohanan <abhinavmohanan018@gmail.com>
2026-01-16 15:56:42 +0530

    align components with light theme

--- nutraleaves/src/components/shared/Header.jsx
+++ nutraleaves/src/components/shared/Header.jsx
@@ -3,14 +3,13 @@ import { LeafIcon } from 'lucide-react'
 
 const Header = () => {
   return (
-    <header className='sticky top-0 z-50 w-full border-b border-emerald-200 dark:border-emerald-900/30 bg-stone-50/90 
-    dark:bg-pearl-900 backdrop-blur-md px-4 lg:px-40 py-3'>
+    <header className='sticky top-0 z-50 w-full border-b border-emerald-200 bg-stone-50/90 backdrop-blur-md px-4 lg:px-40 py-3'>
         <div className='flex items-center justify-between max-w-[1200px] mx-auto'>
-            <div className='flex items-center text-slate-900 dark:text-emerald-400'>
+            <div className='flex items-center gap-2 text-slate-900'>
                 <div className='size-8 text-emerald-500'>
                     <LeafIcon className='w-6 h-6 mt-1'/>
                 </div>
-                <h2 className='text-xl font-black leading-tight tracking-tight dark:text-black'>Nutraleaves</h2>
+                <h2 className='text-xl font-black leading-tight tracking-tight'>Nutraleaves</h2>
             </div>
             <nav className='hidden md:flex items-center gap-6'>
                 <a className='text-sm font-medium hover:text-emerald-500 transition-colors' href="">Home</a>
